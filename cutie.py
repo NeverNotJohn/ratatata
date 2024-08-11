b900f47704a72b77f798d5686d87b5072dba4091
John <johntheherrera@gmail.com>
2024-08-10 17:43:22 -0700

    yay

--- cutie.py
+++ cutie.py
@@ -5,6 +5,10 @@ from sysfont import sysfont
 from machine import SPI,Pin
 import time
 import math
+from hcsr04 import HCSR04
+from time import sleep
+
+
 
 """
 Pinout TFT Display:
@@ -25,11 +29,35 @@ tft=TFT(spi,9,12,13)
 tft.initr()
 tft.rgb(True)
 
+sensor = HCSR04(trigger_pin=0, echo_pin=1, echo_timeout_us=10000)
+
 tft.rotation(3)
 
 def startUp():
+    
+    print('Start!')
+    
     tft.fill(TFT.BLACK)
-    tft.text((0,0), "Hello World!", TFT.WHITE, sysfont, 1)
+    
+    while True:
+        
+        tft.text((5,54), "Scanning...", TFT.WHITE, sysfont, 2)
+        time.sleep(3)
+        
+        if (sensor.distance_cm() < 10):
+            cutie_detected()
+        
+        tft.fill(TFT.BLACK)
+        tft.text((5,54), "Scanning . . . ", TFT.WHITE, sysfont, 2)
+        time.sleep(3)
+        
+        if (sensor.distance_cm() < 10):
+            cutie_detected()
+        
+        tft.fill(TFT.BLACK)
+
+def cutie_detected():
+    print("Cutie Detected!")
     
 # main:
 
